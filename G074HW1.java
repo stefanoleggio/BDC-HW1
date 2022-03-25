0fb0b40c43c3257637b8103e3a68397ba039d41f
stefano28 <stefanoleggio28@gmail.com>
2022-03-25 12:38:01 +0100

    first commit

---
+++ G074HW1.java
@@ -0,0 +1,20 @@
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class G074HW1{
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
