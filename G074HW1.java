85a0cbb2b10fca3ce743d391d78a90f13f947ce8
stefano28 <stefanoleggio28@gmail.com>
2022-03-31 18:43:14 +0200

    spark setup and input reading

--- G074HW1.java
+++ G074HW1.java
@@ -15,6 +15,39 @@ public class G074HW1{
 
     public static void main(String[] args) throws IOException {
 
+        if (args.length != 4) { // Checking the number of the parameters
+            throw new IllegalArgumentException("USAGE: num_partitions num_products country file_path");
+        }
+
+
+        /**
+         *
+         * Spark Setup
+         *
+         */
+
+        SparkConf conf = new SparkConf(true).setAppName("G074HW1");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("WARN");
+
+        /**
+         *
+         * Input Reading
+         *
+         */
+
+        int K = Integer.parseInt(args[0]); //Number of partitions
+        int H = Integer.parseInt(args[1]); //Number of products
+        String S = args[2]; //Country name
+        String file_path = args[3]; //File path
+
+        //Subdivide the input file into K partitions
+        JavaRDD<String> rawData = sc.textFile(file_path).repartition(K).cache();
+
+        //Print the number of rows read from the input file
+        System.out.println("Number of rows read from the input file = " + rawData.count());
+
+        JavaPairRDD<String, Long> productCustomer;
     }
 
 }
